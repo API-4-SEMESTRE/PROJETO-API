541ee2264f66fdff480256e462d06f966dd3266f
Tairik Johnny <tairikjohnny@gmail.com>
2021-09-14 22:36:07 -0300

    Unificando os repositorios, subindo o front-end

---
+++ Sistema/Front-end/api-front/src/views/Fornecedor.vue
@@ -0,0 +1,131 @@
+<template>
+<div>
+    <v-app-bar color="#c74634" dense dark style="border: solid 1px black">
+    <v-avatar>
+      <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" style="position: fixed; right:0px; float:right; margin-bottom: 0;">      
+    </v-avatar>
+
+    </v-app-bar>
+ 
+    <v-app>
+            <v-main >
+                <v-form style="padding-top: 30px">
+                    <v-container class= "ma-70" style="width:40%; border:solid  1px" >
+                            <v-row justify="center" align="stretch" >
+                                <v-col cols="24">
+                                    <span style="padding-top: 8px"> Nome da Empresa </span>
+                                    <v-text-field v-model="empresa" outlined :rules="rules" dense >  </v-text-field>
+                                </v-col>
+                            </v-row>
+                            
+                            <v-row>
+                                <v-col cols="6">
+                                    <span> CNPJ</span>
+                                    <v-text-field v-model="cnpj" outlined  dense>  </v-text-field>
+                                </v-col>
+                                <v-col cols="6">
+                                    <span> Ramo de Atividade </span>
+                                    <v-text-field v-model="ramo" outlined  dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row>
+                                <v-col cols="6">
+                                    <span> CEP </span>
+                                    <v-text-field v-model="cep" outlined  dense>  </v-text-field>
+                                </v-col>
+                                <v-col cols="6">
+                                    <span> Rua </span>
+                                    <v-text-field v-model="rua" outlined  dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row>
+                                <v-col cols="6" md="4">
+                                    <span> Bairro</span>
+                                    <v-text-field v-model="bairro" outlined  dense>  </v-text-field>
+                                </v-col>
+                                <v-col cols="6" md="4">
+                                    <span> Cidade </span>
+                                    <v-text-field v-model="cidade" outlined  dense>  </v-text-field>
+                                </v-col>
+                                 <v-col cols="6" md="4">
+                                    <span> Estado </span>
+                                    <v-text-field v-model="estado" outlined  dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row>
+                                <v-col cols="6">
+                                    <span> Numero </span>
+                                    <v-text-field v-model="numero" outlined  dense>  </v-text-field>
+                                </v-col>
+                                <v-col cols="6">
+                                    <span> Complemento </span>
+                                    <v-text-field v-model="complemento" outlined  dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <p> Contato responsável </p>
+
+                            <v-row justify="center" align="stretch" >
+                                <v-col cols="24">
+                                    <span> Nome Completo</span>
+                                    <v-text-field v-model="nome" outlined :rules="rules" dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row>
+                                <v-col cols="6">
+                                    <span> Função </span>
+                                    <v-text-field v-model="cargo" outlined  dense>  </v-text-field>
+                                </v-col>
+                                <v-col cols="6">
+                                    <span> Telefone </span>
+                                    <v-text-field v-model="telefone" outlined  dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+
+                            <v-row justify="center" align="stretch" >
+                                <v-col cols="24">
+                                    <span> Email</span>
+                                    <v-text-field v-model="email" outlined :rules="rules" dense>  </v-text-field>
+                                </v-col>
+                            </v-row>
+                              
+                            <v-btn class="mr-4"  type="submit"  style="display: flex; justify-content: flex-end; align-items: center; margin: auto; background-color: #c74634; color: white; border:solid 1px black">
+                                Salvar
+                            </v-btn>
+                            
+                    </v-container>
+                </v-form>
+            </v-main>
+    </v-app>
+</div>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        empresa: '',
+        cnpj:'',
+        ramo:'',
+        cep:'',
+        rua:'',
+        bairro:'',
+        cidade:'',
+        estado:'',
+        numero:'',
+        complemento:''
+
+     
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ Sistema/Front-end/api-front/src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+   <v-app id="login">
+      <v-content>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-card-text>
+                        <v-form>
+                           <v-text-field
+                              name="login"
+                              label="Login"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field
+                              id="password"
+                              name="password"
+                              label="Senha"
+                              type="password"
+                           ></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>           
+                        <v-btn block color="#c74634" to="/home">Login</v-btn>
+                     </v-card-actions>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-content>
+   </v-app>
+</template>
+
+<script>
+export default {
+    name: "login",
+    data() {
+        return {};
+    },
+    methods: {}
+}
+</script>
