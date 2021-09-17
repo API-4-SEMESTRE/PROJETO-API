59fa71db5b13efef9c3201a221f2b7922f13ee94
Tairik Johnny <tairikjohnny@gmail.com>
2021-09-16 22:23:09 -0300

    API-23 Arrumando as rotas pro arquivo Cadastro_Fornecedor

--- Sistema/api-front/src/router/index.js
+++ Sistema/api-front/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-import Fornecedor from '../views/Fornecedor'
+import Cadastro_Fornecedor from '../views/Cadastro_Fornecedor'
 import Cadastro_Usuario from '../views/Cadastro_Usuario'
 
 Vue.use(VueRouter)
@@ -27,9 +27,9 @@ const routes = [
     component: Login
   },
   {
-    path: '/fornecedor',
-    name: 'Fornecedor',
-    component: Fornecedor
+    path: '/Cadastro_fornecedor',
+    name: 'Cadastro_Fornecedor',
+    component: Cadastro_Fornecedor
   },
   {
     path: '/Cadastro_Usuario',
