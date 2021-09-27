e96aa6ec3853b26905d311b91c7787f2de72b21f
Tairik Johnny <tairikjohnny@gmail.com>
2021-09-27 17:03:31 -0300

    API-40 Trabalhando em cima da CRUD da tela de cadastro de usuario

--- Sistema/api-front/src/views/Cadastro_Usuario.vue
+++ Sistema/api-front/src/views/Cadastro_Usuario.vue
@@ -66,6 +66,142 @@
         </v-form>
       </v-main>
     </v-app>
+    <!--DATA TABLE-->
+    <v-card class="pa-2" tile outlined color="#DCDCDC" style="margin-top: 30px">
+      <v-card-text>
+        <v-card>
+          <v-card-title>
+            Lista de Usuários
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Pesquisar"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headers1"
+            :items="lista_de_usuarios"
+            :search="search"
+          ></v-data-table>
+        </v-card>
+      </v-card-text>
+    </v-card>
+    <template>
+      <v-data-table
+        :headers="headers"
+        :items="lista_de_usuarios"
+        sort-by="calories"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>Lista de Usuários</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  Novo Usuário
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row justify="center">
+                      <v-col cols="24">
+                        <v-text-field
+                          label="Nome Completo"
+                          v-model="usuario.nome"
+                          :rules="regra_nome"
+                          outlined
+                          required
+                          dense
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center">
+                      <v-col cols="24">
+                        <v-text-field
+                          label="E-mail"
+                          v-model="usuario.email"
+                          :rules="regra_email"
+                          outlined
+                          required
+                          dense
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center">
+                      <v-col cols="24">
+                        <v-text-field
+                          label="Senha"
+                          v-model="usuario.senha"
+                          :rules="regra_senha"
+                          outlined
+                          required
+                          dense
+                          password
+                          type="password"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">
+                    Cancelar
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="save">
+                    Salvar
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="540px">
+              <v-card>
+                <v-card-title class="text-h5"
+                  >Tem certeza de que deseja excluir este item?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"
+                    >Cancelar</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                    >Sim</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
+    </template>
   </div>
 </template>
 
@@ -86,6 +222,9 @@ export default {
     ],
     regra_senha: [(v) => !!v || "A senha é obrigatória"],
 
+    // Array com a lista de usuarios
+    lista_de_usuarios: [],
+
     // Criando o objeto que vai ser feito o POST
     usuario: {
       nome: "",
@@ -94,7 +233,78 @@ export default {
       active: "True",
       senha: "",
     },
+
+    // Array que contem as colunas da tabela
+    headers1: [
+      {
+        text: "CÓDIGO",
+        align: "start",
+        value: "cod",
+      },
+      { text: "NOME", value: "nome" },
+      { text: "TIPO", value: "tipo" },
+      { text: "EMAIL", value: "email" },
+      { text: "DATA DE CRIAÇÃO", value: "date_create" },
+      { text: "ATIVO", value: "active" },
+      //{ text: "SENHA", value: "senha" },
+    ],
+
+    // DATA TABLE
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "CÓDIGO",
+        align: "start",
+        value: "cod",
+      },
+      { text: "NOME", value: "nome" },
+      { text: "TIPO", value: "tipo" },
+      { text: "EMAIL", value: "email" },
+      { text: "DATA DE CRIAÇÃO", value: "date_create" },
+      { text: "ATIVO", value: "active" },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
   }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  mounted() {
+    this.exibir_usuario();
+  },
 
   methods: {
     // Método de cadastro de usuario
@@ -119,9 +329,145 @@ export default {
           );
         });
     },
+    // Método pra exibir os usuarios
+    exibir_usuario() {
+      Usuario.listar_usuarios()
+        .then((resposta_lista_usuarios) => {
+          this.lista_de_usuarios = resposta_lista_usuarios.data;
+        })
+        .catch((e) => {
+          Swal.fire(
+            "Oops...",
+            "Erro ao carregar a tabela de usuários! - Erro: " +
+              e.response.data.error,
+            "error"
+          );
+        });
+    },
     // Método que valida se os campos estão preenchidos, se não estiverem ele bloqueia o botão CADASTRAR
     validate() {
       this.$refs.form.validate();
+    },
+
+    //DATA TABLE
+    initialize() {
+      this.desserts = [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.lista_de_usuarios.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.lista_de_usuarios.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.lista_de_usuarios.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(
+          this.lista_de_usuarios[this.editedIndex],
+          this.editedItem
+        );
+      } else {
+        this.lista_de_usuarios.push(this.editedItem);
+      }
+      this.close();
     },
   },
 };
