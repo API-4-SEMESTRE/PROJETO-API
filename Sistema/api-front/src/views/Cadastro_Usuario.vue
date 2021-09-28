155dc4ea22fb772dc6612fc485dddf723acac814
Tairik Johnny <tairikjohnny@gmail.com>
2021-09-28 12:19:35 -0300

    API-40 Recuperando os dados da tabela pra fazer a ediçao do usuario

--- Sistema/api-front/src/views/Cadastro_Usuario.vue
+++ Sistema/api-front/src/views/Cadastro_Usuario.vue
@@ -174,21 +174,6 @@ export default {
       senha: "",
     },
 
-    // Array que contem as colunas da tabela
-    headers1: [
-      {
-        text: "CÓDIGO",
-        align: "start",
-        value: "cod",
-      },
-      { text: "NOME", value: "nome" },
-      { text: "TIPO", value: "tipo" },
-      { text: "EMAIL", value: "email" },
-      { text: "DATA DE CRIAÇÃO", value: "date_create" },
-      { text: "ATIVO", value: "active" },
-      //{ text: "SENHA", value: "senha" },
-    ],
-
     // DATA TABLE
     dialog: false,
     dialogDelete: false,
@@ -208,18 +193,20 @@ export default {
     desserts: [],
     editedIndex: -1,
     editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      cod: 0,
+      nome: "",
+      tipo: "",
+      email: "",
+      date_create: "",
+      active: "",
     },
     defaultItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      cod: 0,
+      nome: "",
+      tipo: "",
+      email: "",
+      date_create: "",
+      active: "",
     },
   }),
 
@@ -291,84 +278,14 @@ export default {
     },
 
     //DATA TABLE
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
-      ];
+
+    editarUsuario(usuario) {
+      this.usuario = usuario;
     },
 
-    editItem(item) {
-      this.editedIndex = this.lista_de_usuarios.indexOf(item);
-      this.editedItem = Object.assign({}, item);
+    editItem(usuario) {
+      this.usuario = this.lista_de_usuarios.indexOf(usuario);
+      this.usuario = Object.assign({}, usuario);
       this.dialog = true;
     },
 
