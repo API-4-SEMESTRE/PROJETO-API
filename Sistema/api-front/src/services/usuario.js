4880292b47b12e4e0faa06a0ab35dceb740c66d3
Tairik Johnny <tairikjohnny@gmail.com>
2021-10-01 22:42:07 -0300

    API-40 Criando a chamada pra excluir o usuario

--- Sistema/api-front/src/services/usuario.js
+++ Sistema/api-front/src/services/usuario.js
@@ -1,15 +1,16 @@
-import { http } from './config'
+import { http } from "./config";
 
 export default {
-
-    listar_usuarios: () => {
-        return http.get(`usuario/all`)
-    },
-    salvar_usuario: (usuario) => {
-        return http.post(`usuario/add`, usuario)
-    },
-    atualizar_usuario: (usuario) => {
-        return http.put('usuario/update', usuario)
-    }
-
-}
+  listar_usuarios: () => {
+    return http.get(`usuario/all`);
+  },
+  salvar_usuario: (usuario) => {
+    return http.post(`usuario/add`, usuario);
+  },
+  atualizar_usuario: (usuario) => {
+    return http.put(`usuario/update`, usuario);
+  },
+  excluir_usuario: (usuario) => {
+    return http.delete(`usuario/delete`, { data: usuario });
+  },
+};
