83dc2117cc6447881d3f1c5408075ef309efaf59
Tairik Johnny <tairikjohnny@gmail.com>
2021-10-05 23:57:15 -0300

    API-83 Estilizando a tela de Criar Conta

--- Sistema/api-front/src/views/Criar_Conta.vue
+++ Sistema/api-front/src/views/Criar_Conta.vue
@@ -1,93 +1,100 @@
 <template>
-  <div>
+  <v-app id="login">
     <v-content>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-card-title>
-                <span class="text-h5">Criar Conta</span>
-              </v-card-title>
-
+            <h1 style="text-align: center; margin-bottom: 20px; color: white">
+              Criar Conta
+            </h1>
+            <v-card class="elevation-12" id="card-login">
               <v-card-text>
-                <v-container>
-                  <v-form
-                    ref="form"
-                    v-model="valid"
-                    lazy-validation
-                    @submit.prevent="cadastrar_usuario"
-                  >
-                    <v-container>
-                      <v-row justify="center">
-                        <v-col cols="24">
-                          <v-text-field
-                            label="Nome Completo"
-                            v-model="usuario.nome"
-                            :rules="regra_nome"
-                            outlined
-                            required
-                            dense
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row justify="center">
-                        <v-col cols="24">
-                          <v-text-field
-                            label="E-mail"
-                            v-model="usuario.email"
-                            :rules="regra_email"
-                            outlined
-                            required
-                            dense
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row justify="center">
-                        <v-col cols="24">
-                          <v-text-field
-                            label="Senha"
-                            v-model="usuario.senha"
-                            :rules="regra_senha"
-                            outlined
-                            required
-                            dense
-                            password
-                            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                            :type="show1 ? 'text' : 'password'"
-                            @click:append="show1 = !show1"
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col></v-col>
-                        <v-col>
-                          <v-btn block color="error" :to="{ name: 'Login' }"
-                            >Cancelar</v-btn
-                          >
-                        </v-col>
-                        <v-col>
-                          <v-btn
-                            class="mr-4"
-                            type="submit"
-                            :disabled="!valid"
-                            @click="validate"
-                            id="btn_cadastrar_usuario"
-                            color="primary"
-                          >
-                            Salvar
-                          </v-btn>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </v-form>
-                </v-container>
+                <v-form
+                  ref="form"
+                  v-model="valid"
+                  lazy-validation
+                  @submit.prevent="cadastrar_usuario"
+                >
+                  <v-container>
+                    <v-row justify="center">
+                      <v-col cols="24">
+                        <span style="color: white; font-size: 18px">Nome</span>
+                        <v-text-field
+                          label="Nome"
+                          v-model="usuario.nome"
+                          :rules="regra_nome"
+                          single-line
+                          solo
+                          required
+                          dense
+                          background-color="#A9A9A9"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center">
+                      <v-col cols="24">
+                        <span style="color: white; font-size: 18px"
+                          >E-mail</span
+                        >
+                        <v-text-field
+                          label="E-mail"
+                          v-model="usuario.email"
+                          :rules="regra_email"
+                          single-line
+                          solo
+                          required
+                          dense
+                          background-color="#A9A9A9"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center">
+                      <v-col cols="24">
+                        <span style="color: white; font-size: 18px">Senha</span>
+                        <v-text-field
+                          label="Senha"
+                          v-model="usuario.senha"
+                          :rules="regra_senha"
+                          background-color="#A9A9A9"
+                          single-line
+                          solo
+                          required
+                          dense
+                          password
+                          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                          :type="show1 ? 'text' : 'password'"
+                          @click:append="show1 = !show1"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col></v-col>
+                      <v-col>
+                        <v-btn text color="white" :to="{ name: 'Login' }"
+                          >Cancelar</v-btn
+                        >
+                      </v-col>
+                      <v-col>
+                        <v-btn
+                          color="#C84634"
+                          class="white--text mr-4"
+                          type="submit"
+                          :disabled="!valid"
+                          @click="validate"
+                        >
+                          Salvar
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-form>
               </v-card-text>
             </v-card>
           </v-flex>
         </v-layout>
       </v-container>
     </v-content>
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -151,11 +158,7 @@ export default {
 </script>
 
 <style>
-#btn_cadastrar_usuario {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
-  border: solid 1px black;
+#criar-conta {
+  background-color: #181820;
 }
 </style>
