a316e1598512fe92766bd2c90d50990212b2ae12
Tairik Johnny <tairikjohnny@gmail.com>
2021-10-28 21:43:10 -0300

    API-95 Consumindo os endpoints de update e delete

--- Sistema/api-front/src/services/fornecedor.js
+++ Sistema/api-front/src/services/fornecedor.js
@@ -5,8 +5,17 @@ export default {
     listar_fornecedor: () => {
         return http.get(`fornecedor/listaFornecedores`)
     },
+    listar_fornecedor_all: () => {
+        return http.get(`fornecedor/all`)
+    },
     salvar_fornecedor: (fornecedor) => {
         return http.post(`fornecedor/add`, fornecedor)
-    }
+    },
+    atualizar_fornecedor: (fornecedor) => {
+        return http.put(`fornecedor/update`, fornecedor);
+    },
+    excluir_fornecedor: (fornecedor) => {
+        return http.delete(`fornecedor/delete`, { data: fornecedor });
+    },
 
 }
