bf60dbea77d17d766b5d7142bfb3371b92ebe8d3
Tairik Johnny <tairikjohnny@gmail.com>
2021-11-07 01:18:08 -0300

    API-88 Consumindo os endpoints da parte de adicionar o fornecedor ao evento

--- Sistema/api-front/src/services/evento.js
+++ Sistema/api-front/src/services/evento.js
@@ -1,6 +1,7 @@
 import { http } from "./config";
 
 export default {
+    // ENDPOINT EVENTO
     listar_eventos: () => {
         return http.get(`evento/all`);
     },
@@ -12,5 +13,15 @@ export default {
     },
     excluir_evento: (evento) => {
         return http.delete(`evento/delete`, { data: evento });
+    },
+    // ENDPOINT FORNECEDOR EVENTO
+    listar_fornecedor_eventos: () => {
+        return http.get(`fornecedorEvento/all`);
+    },
+    salvar_fornecedor_evento: (fornecedor_evento) => {
+        return http.post(`fornecedorEvento/add`, fornecedor_evento);
+    },
+    atualizar_fornecedor_evento: (fornecedor_evento) => {
+        return http.put(`fornecedorEvento/update`, fornecedor_evento);
     },
 };
