a53966108775a67d88e51b6c17f2964467a3d43d
Tairik Johnny <tairikjohnny@gmail.com>
2021-11-12 22:29:35 -0300

    API-124 Criando a tela de relatorios

---
+++ Sistema/api-front/src/views/Relatorios.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-app id="relatorios">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md12>
+            <h1 style="text-align: center; color: white; margin-top: 15px">
+              Relatórios
+            </h1>
+            <v-layout align-center justify-center>
+              <div>
+                <v-card
+                  class="pa-2"
+                  tile
+                  outlined
+                  color="#272733"
+                  style="margin-top: 20px"
+                >
+                  <v-card-text>
+                    <template>
+                      <v-dialog v-model="dialog" max-width="600px">
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-btn
+                            color="#C84634"
+                            class="white--text"
+                            dark
+                            v-bind="attrs"
+                            v-on="on"
+                          >
+                            Gerar CSV
+                          </v-btn>
+                        </template>
+                        <v-card style="background-color: #272733">
+                          <v-card-title>
+                            <span class="text-h5 white--text"
+                              >Escolher mês</span
+                            >
+                          </v-card-title>
+                          <v-card-text>
+                            <v-container>
+                              <v-form
+                                ref="form"
+                                v-model="valid"
+                                lazy-validation
+                                @submit.prevent=""
+                              >
+                                <v-container>
+                                  <v-row justify="center">
+                                    <v-col cols="24">
+                                      <template>
+                                        <v-row justify="center">
+                                          <v-date-picker
+                                            v-model="picker"
+                                            type="month"
+                                            locale="brasil"
+                                          ></v-date-picker>
+                                        </v-row>
+                                      </template>
+                                    </v-col>
+                                  </v-row>
+                                  <v-row>
+                                    <v-col></v-col>
+                                    <v-col>
+                                      <v-btn text color="white" @click="close">
+                                        Cancelar
+                                      </v-btn>
+                                    </v-col>
+                                    <v-col>
+                                      <v-btn
+                                        color="#C84634"
+                                        class="white--text mr-4"
+                                        type="submit"
+                                        :disabled="!valid"
+                                        @click="validate"
+                                      >
+                                        Salvar
+                                      </v-btn>
+                                    </v-col>
+                                  </v-row>
+                                </v-container>
+                              </v-form>
+                            </v-container>
+                          </v-card-text>
+                        </v-card>
+                      </v-dialog>
+                    </template>
+                  </v-card-text>
+                </v-card>
+              </div>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: () => ({
+    valid: true,
+    dialog: false,
+    picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    // Método que vai fechar o modal "dialog"
+    close() {
+      this.dialog = false;
+      this.evento = {};
+    },
+  },
+};
+</script>
+
+<style>
+#relatorios {
+  background-color: #181820;
+}
+</style>
